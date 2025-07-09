a41384adc438364771091ae3852d1b474f8222ef
Araelka <RoseRedOwl@gmail.com>
2025-07-09 08:42:48 +0700

    modified:   app/Services/MarkdownService.php

--- app/Services/MarkdownService.php
+++ app/Services/MarkdownService.php
@@ -21,33 +21,26 @@ class MarkdownService
         $content = $markdown;
 
         $content = preg_replace_callback('/!\[(.*?)\]\((.*?)\)\{(.+?)\}/', function ($matches) {
-            static $idCounter = 0;
             $altText = $matches[1];
             $imageSrc = $matches[2];
             $params = $matches[3];
 
-            $uniqueId = 'img-' . $idCounter++;
-
-            $this->tempParams[$imageSrc] = [
-                'src' => $imageSrc,
-                'params' => $params,
-            ];
+            $this->tempParams[$imageSrc] = $params;
 
             return "![{$altText}]({$imageSrc})";
         }, $markdown);
 
-        dd($this->tempParams);
+        // dd($this->tempParams);
 
         $content = preg_replace('/<br>/m', "\n&nbsp;\n", $content);
 
         $content = $this->parsedown->text($content);
 
-        $content = preg_replace_callback('/<img\s+src="([^"]+)"\s+alt="([^"]+)"\s*data-id="([^"]+)"\s*\/?>/', function ($matches) {
+        $content = preg_replace_callback('/<img\s+src="([^"]+)"\s+alt="([^"]+)"\s*\/?>/', function ($matches) {
             $imageSrc = htmlspecialchars_decode($matches[1], ENT_QUOTES);
             $altText = htmlspecialchars_decode($matches[2], ENT_QUOTES);
-            $uniqueId = $matches[3];
 
-            $params = $this->tempParams[$uniqueId]['params'] ?? '';
+            $params = $this->tempParams[$imageSrc] ?? '';
             $attributes = [];
             if ($params) {
                 preg_match_all('/(\w+)=([\'"]?)([^\'"\s]+)\2/', $params, $paramMatches, PREG_SET_ORDER);
@@ -70,5 +63,4 @@ class MarkdownService
 
         return $content;
     }
-
 }
